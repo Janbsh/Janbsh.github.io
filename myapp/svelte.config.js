2a7baaec466e5ab1e3da5aae6a79746aaa3ace2f
Janbsh <janbshemail@gmail.com>
2024-08-23 15:56:26 -0500

    Created Project

---
+++ myapp/svelte.config.js
@@ -0,0 +1,24 @@
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const dev = process.env.NODE_ENV === 'development';
+
+const config = {
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+
+	kit: {
+		adapter: adapter({
+			pages: 'docs',
+			assets: 'docs',
+		}),
+
+		paths: {
+			base: dev ? '' : '/janbsh.github.io',
+			relative: false
+		}
+	}
+};
+
+export default config;
