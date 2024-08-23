34eaff2dc736acb12295fc442ce36514129ce5b9
Janbsh <janbshemail@gmail.com>
2024-08-23 17:17:04 -0500

    Created Project

--- myapp/svelte.config.js
+++ myapp/svelte.config.js
@@ -3,6 +3,7 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const dev = process.env.NODE_ENV === 'development';
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
